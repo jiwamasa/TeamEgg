6c5ee0b0cfe8941905fe87539af6120ab5b7ae79
James Takeshi Iwamasa <jiwamasa@ucsc.edu>
2019-01-25 22:35:01 -0800

    Created Base Speech Bubble System
    'SpeechBubbler.cs' : Objects that can spawn speech bubbles. Call 'SpawnSpeechBubble' to create a speech bubble.

---
+++ DuckGame/Assets/SpeechBubble/Scripts/SpeechBubble.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Single speech bubble
+public class SpeechBubble : MonoBehaviour
+{
+
+	
+}
---
+++ DuckGame/Assets/SpeechBubble/Scripts/SpeechBubbler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// Attach to object that can spawn speech bubbles
+public class SpeechBubbler : MonoBehaviour
+{
+    public Canvas speechCanvas; // Main canvas for speech bubbles
+    public GameObject speechBubblePrefab; // Prefab of speech bubble object
+
+    void Start()
+    {
+        SpawnSpeechBubble("hello world", 2f); // TEST
+    }
+
+    // Spawn a speech bubble
+    // IN:
+    //   speechText: Text to print
+    //   speechTime: Time before speech bubble disappears
+    public void SpawnSpeechBubble(string speechText, float speechTime)
+    {        
+        // Create speech bubble
+        GameObject speechBubble = Instantiate(speechBubblePrefab, speechCanvas.transform);
+        // Set speech bubble position
+        speechBubble.transform.position = Camera.main.WorldToScreenPoint(transform.position);
+        // Set speech time (which actually sets speed, so we use the inverse)
+        speechBubble.GetComponent<Animator>().SetFloat("SpeechTime", 1f / speechTime);
+        // Set speech text
+        speechBubble.GetComponentInChildren<Text>().text = speechText;
+    }
+}
