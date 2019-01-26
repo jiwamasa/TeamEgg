076020db029bddfc395925988149334eb09b43f1
Justin Law <jlawhome08@gmail.com>
2019-01-26 14:03:51 -0800

    Fishing Controls
    Can fish up an object with a fish component.

---
+++ DuckGame/Assets/FishBehavior.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishBehavior : MonoBehaviour {
+
+    Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float dist = Vector2.Distance(this.transform.position, Vector2.zero);
+		if (dist < 1f){
+            rb.isKinematic = false;
+            rb.AddForce(new Vector2 (0f, 3f), ForceMode2D.Impulse);
+            //Camera.main.transform.parent = this.transform;
+        }
+	}
+}
---
+++ DuckGame/Assets/FishingControls.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishingControls : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    private Vector2 velocity = Vector2.zero;
+    private string state = "reel";
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        state = Input.GetKey("space") ? "cast" : "reel";
+
+        if (state == "reel") {
+            transform.position = Vector2.SmoothDamp(transform.position, Vector2.zero, ref velocity, 0.1f);
+            rb.velocity = Vector2.zero;
+            float dist = Vector2.Distance(transform.position, Vector2.zero);
+        } else if (state == "cast") {
+
+        }
+
+
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (state == "reel" && other.GetComponent<FishBehavior>())
+        {
+            //share position
+            other.transform.parent = this.transform;
+
+            //share velocity
+            //FixedJoint2D joint = other.gameObject.GetComponent<FixedJoint2D>();
+            //joint.connectedBody = rb;
+            //joint.enabled = true;
+        }
+    }
+
+}
