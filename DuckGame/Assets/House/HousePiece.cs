e303231fa621ca3998af84b118d799eaf45e74fe
bion719 <dan.j.chamberlin@gmail.com>
2019-01-26 15:02:12 -0800

    Duck In the Game

---
+++ DuckGame/Assets/House/HousePiece.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Room that a character can live in
+public class HousePiece : MonoBehaviour
+{
+
+}
---
+++ DuckGame/Assets/SpeechBubble/Scripts/SpeechBubbler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// Attach to object that can spawn speech bubbles
+public class SpeechBubbler : MonoBehaviour
+{
+    public GameObject speechBubblePrefab; // Prefab of speech bubble object
+
+    // Spawn a speech bubble with an image
+    // IN:
+    //   speechImage: Sprite to display
+    //   speechTime: Time before speech bubble disappears
+    public void SpawnSpeechBubble(AnimationClip emote, float speechTime)
+    {
+        // Create speech bubble
+        GameObject speechBubble = Instantiate(speechBubblePrefab, transform);
+        // Set speech bubble position
+        speechBubble.transform.localPosition = Vector3.zero;
+        // Set speech time (which actually sets speed, so we use the inverse)
+        speechBubble.GetComponent<Animator>().SetFloat("SpeechTime", 1f / speechTime);
+        // Set emote
+        speechBubble.GetComponentInChildren<SpeechBubble>().emoteController["Emote"] = emote;
+    }
+}
