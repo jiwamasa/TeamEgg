0b124e0844b6fc959c4358a8de0486fbef1a4852
groverburger <groverburger696@gmail.com>
2019-01-25 22:11:23 -0800

    water item spawning first pass

---
+++ DuckGame/Assets/Scripts/FloatyMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatyMove : MonoBehaviour 
+{
+    public float speed;
+    public float timer = 2.0f;
+
+    // Use this for initialization
+    void Start() {}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        timer -= Time.deltaTime;
+        transform.Translate(speed, 0.0f, 0.0f);
+
+        if (timer <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+}
---
+++ DuckGame/Assets/Scripts/FloatySpawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatySpawn : MonoBehaviour 
+{
+    public float timer = 0.0f;
+    public float timeInterval = 1.0f;
+    public GameObject spawnable; 
+
+	// Use this for initialization
+	void Start () {}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= timeInterval)
+        {
+            timer -= timeInterval;
+            Instantiate(spawnable, new Vector2(-10.0f,Constants.WaterLevel), Quaternion.identity);
+        }
+	}
+}
