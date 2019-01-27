9cb8d6a7b49611467b239b7645c64611d5889009
groverburger <groverburger696@gmail.com>
2019-01-26 16:19:25 -0800

    random selection of floaties

--- DuckGame/Assets/Scripts/FloatySpawn.cs
+++ DuckGame/Assets/Scripts/FloatySpawn.cs
@@ -2,22 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FloatySpawn : MonoBehaviour 
+public class FloatySpawn : MonoBehaviour
 {
     public float timer = 0.0f;
     public float timeIntervalMin = 3.0f;
     public float timeIntervalMax = 10.0f;
     private float randomInterval;
-    public GameObject spawnable; 
+    public GameObject spawnable;
+    public List<string> names = new List<string>();
 
 	// Use this for initialization
-	void Start () 
+	void Start ()
     {
         GenerateInterval();
     }
-	
+
 	// Update is called once per frame
-	void Update () 
+	void Update ()
     {
         timer += Time.deltaTime;
 
@@ -25,7 +26,10 @@ public class FloatySpawn : MonoBehaviour
         {
             timer -= randomInterval;
             GenerateInterval();
-            Instantiate(spawnable, new Vector2(-10.0f,Constants.WaterLevel), Quaternion.identity);
+            int randomIndex;
+            randomIndex = Random.Range(0, names.Count);
+            GameObject instance = Instantiate(spawnable, new Vector2(-10.0f,Constants.WaterLevel), Quaternion.identity);
+            instance.GetComponent<SpriteRenderer>().sprite = Resources.Load(names[randomIndex], typeof(Sprite)) as Sprite;
         }
 	}
 
