79af7f518a2d680f8874b01a8af1b6ebc264747d
bion719 <dan.j.chamberlin@gmail.com>
2019-01-26 17:15:33 -0800

    Added title scene and scene transitions

---
+++ DuckGame/Assets/Title and Intro/TitleController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// Manages title controls
+public class TitleController : MonoBehaviour
+{
+    public string introCutscene; // Starting cutscene
+
+    // Start game
+    public void StartGame()
+    {
+        SceneManager.LoadScene(introCutscene);
+    }
+
+    // Quit game
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+} 
+
